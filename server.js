eae38d5f151fb8d7c2c2094d614c98ce757f7969
Mahesh <mahesh.deploy@gmail.com>
2019-08-14 11:05:45 -0700

    commit

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors);
-
 app.get('/', (req, res) => {
 	res.send([
 		{
@@ -238,6 +236,8 @@ app.get('/', (req, res) => {
 		}
 	]);
 });
+
+app.use(cors);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
