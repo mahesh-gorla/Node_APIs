00a12f3c1cd88c694ebba9a81389e15c5139d016
Mahesh <mahesh.deploy@gmail.com>
2019-08-14 11:19:20 -0700

    commiting

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+	res.header('Content-Type', 'application/json');
 	res.send([
 		{
 			id: 1,
